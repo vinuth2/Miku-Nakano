373f33babdb47c752e16c20e798b11828d91fcc1
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-19 01:30:31 +0530

    Update __init__.py

--- MikuXProBot/__init__.py
+++ MikuXProBot/__init__.py
@@ -264,7 +264,7 @@ BOT_NAME = bottie.first_name
 BOT_MENTION = bottie.mention
 
 if "@MikuXProBot" not in PM_START_TEXT:
-    LOGGER.critical(f"{OWNER_ID} Is Cheating. Add `Thanks To @MikuXProBot For Repo` In PM_START_TEXT To Fix This")
+    LOGGER.critical(f"{OWNER_ID} Is Cheating. Add `Hit /help to find out more about how to use me to my full potential.` In PM_START_TEXT To Fix This")
     sys.exit(1)
 else:
     LOGGER.info("Your Bot Is Ready")
